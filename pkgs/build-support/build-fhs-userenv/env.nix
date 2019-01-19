1b94369b315e5de52ac6593beb42d06c162641ae
Tor Hedin Brønner <torhedinbronner@gmail.com>
2019-01-19 14:09:53 +0100

    don't set LD_LIBRARY_PATH

--- pkgs/build-support/build-fhs-userenv/env.nix
+++ pkgs/build-support/build-fhs-userenv/env.nix
@@ -50,7 +50,7 @@ with pkgs.lib; let
   etcProfile = writeText "profile" ''
     export PS1='${name}-chrootenv:\u@\h:\w\$ '
     export LOCALE_ARCHIVE='/usr/lib/locale/locale-archive'
-    export LD_LIBRARY_PATH='/run/opengl-driver/lib:/run/opengl-driver-32/lib:/usr/lib:/usr/lib32'
+    # export LD_LIBRARY_PATH='/run/opengl-driver/lib:/run/opengl-driver-32/lib:/usr/lib:/usr/lib32'
     export PATH="/run/wrappers/bin:/usr/bin:/usr/sbin${optionalString inheritPath "\${PATH:-:}$PATH"}"
     export TZDIR='/etc/zoneinfo'
 
