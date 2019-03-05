946da402348b2e9ab17f5ee1e5b0e8461a97aee2
Tor Hedin Brønner <torhedinbronner@gmail.com>
2019-03-05 22:31:16 +0100

    gupnp: 1.0.3 -> 1.1.2
    Update Requires patch for meson

--- pkgs/development/libraries/gupnp/default.nix
+++ pkgs/development/libraries/gupnp/default.nix
@@ -1,14 +1,16 @@
-{ stdenv, fetchurl, pkgconfig, gobject-introspection, vala, gtk-doc, docbook_xsl, docbook_xml_dtd_412, docbook_xml_dtd_44, glib, gssdp, libsoup, libxml2, libuuid }:
+{ stdenv, fetchurl, pkgconfig, gobject-introspection, vala, gtk-doc
+, docbook_xsl, docbook_xml_dtd_412, docbook_xml_dtd_44, glib, gssdp, libsoup
+, libxml2, libuuid, meson, ninja }:
 
 stdenv.mkDerivation rec {
   name = "gupnp-${version}";
-  version = "1.0.3";
+  version = "1.1.2";
 
   outputs = [ "out" "dev" "devdoc" ];
 
   src = fetchurl {
     url = "mirror://gnome/sources/gupnp/${stdenv.lib.versions.majorMinor version}/gupnp-${version}.tar.xz";
-    sha256 = "1fyb6yn75vf2y1b8nbc1df572swzr74yiwy3v3g5xn36wlp1cjvr";
+    sha256 = "04nlhg7si6in2r3lhn9il7drw62m6m1f9ai013m9133ax27pq2ih";
   };
 
   patches = [
@@ -18,14 +20,21 @@ stdenv.mkDerivation rec {
     # https://bugzilla.gnome.org/show_bug.cgi?id=685477
     # at least until Requires.internal or something is implemented
     # https://gitlab.freedesktop.org/pkg-config/pkg-config/issues/7
+
+    # Meson update:
+    # Add propagatedBuildInputs to pkg-config Requires field
     ./fix-requires.patch
   ];
 
-  nativeBuildInputs = [ pkgconfig gobject-introspection vala gtk-doc docbook_xsl docbook_xml_dtd_412 docbook_xml_dtd_44 ];
+  nativeBuildInputs = [
+    meson ninja
+    pkgconfig gobject-introspection vala gtk-doc docbook_xsl
+    docbook_xml_dtd_412 docbook_xml_dtd_44
+  ];
   propagatedBuildInputs = [ glib gssdp libsoup libxml2 libuuid ];
 
-  configureFlags = [
-    "--enable-gtk-doc"
+  mesonFlags = [
+    "-Dgtk_doc=true"
   ];
 
   doCheck = true;
